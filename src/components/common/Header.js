5ce945bb5bfa9d8749efc62a07f9bc7cefa977b9
Cory House <housecor@gmail.com>
2017-08-13 21:01:05 -0500

    Use bootstrap navbar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-  const activeStyle = { color: 'blue' };
+  const activeStyle = { color: 'orange' };
   return (
-    <nav>
-      <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
-      {" | "}
-      <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>
-      {" | "}
-      <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">PS Admin</NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/courses" activeStyle={activeStyle}>Courses</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about" activeStyle={activeStyle}>About</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
